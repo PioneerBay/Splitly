c316552475df5dead8bbffa6becafde78ac8423d
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2025-06-22 12:54:52 -0700

    change screen height

--- composeApp/src/desktopMain/kotlin/com/pioneerbay/splitly/main.kt
+++ composeApp/src/desktopMain/kotlin/com/pioneerbay/splitly/main.kt
@@ -1,13 +1,17 @@
 package com.pioneerbay.splitly
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Splitly",
-    ) {
-        App()
+fun main() =
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "Splitly",
+            state = rememberWindowState(width = 1179.dp / 3, height = 2556.dp / 3),
+        ) {
+            App()
+        }
     }
-}
