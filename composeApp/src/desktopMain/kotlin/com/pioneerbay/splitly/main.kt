ba02679e18d60124e5520f25850eeb1a13992a0b
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2025-06-22 12:59:01 -0700

    feat(main): adjust window size to maintain aspect ratio based on screen dimensions

--- composeApp/src/desktopMain/kotlin/com/pioneerbay/splitly/main.kt
+++ composeApp/src/desktopMain/kotlin/com/pioneerbay/splitly/main.kt
@@ -2,15 +2,24 @@ package com.pioneerbay.splitly
 
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import java.awt.Toolkit
 
 fun main() =
     application {
+        val screenSize = Toolkit.getDefaultToolkit().screenSize
         Window(
             onCloseRequest = ::exitApplication,
             title = "Splitly",
-            state = rememberWindowState(width = 1179.dp / 3, height = 2556.dp / 3),
+            state =
+                rememberWindowState(
+                    width = (screenSize.height * 1179 / 2556).dp, // keep width:height ratio
+                    height = screenSize.height.dp,
+                    position =
+                        WindowPosition(0.dp, 0.dp),
+                ),
         ) {
             App()
         }
