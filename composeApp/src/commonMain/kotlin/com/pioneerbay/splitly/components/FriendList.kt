5fa5dfa67f6010d997a59a4940e1edaa1a37b72d
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2025-06-24 16:55:00 -0700

    Update FriendList.kt

--- composeApp/src/commonMain/kotlin/com/pioneerbay/splitly/components/FriendList.kt
+++ composeApp/src/commonMain/kotlin/com/pioneerbay/splitly/components/FriendList.kt
@@ -1,15 +1,18 @@
 package com.pioneerbay.splitly.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.pioneerbay.splitly.pages.Friend
@@ -83,8 +86,14 @@ private fun FriendItem(friend: Friend) {
         modifier =
             Modifier
                 .fillMaxWidth()
-                .padding(vertical = 4.dp),
-        shape = MaterialTheme.shapes.small,
+                .padding(vertical = 4.dp)
+                .background(Color.White),
+        colors =
+            CardDefaults.cardColors().copy(
+                containerColor = MaterialTheme.colorScheme.surface,
+                contentColor = MaterialTheme.colorScheme.onSurface,
+            ),
+        shape = MaterialTheme.shapes.medium,
     ) {
         Column(
             modifier =
